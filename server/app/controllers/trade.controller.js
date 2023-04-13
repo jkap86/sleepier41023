d40caeb458bac4ee0b3a6fb566e31ce6d3d5b8ce
jkap86 <jkap86@gmail.com>
2023-04-13 06:31:11 -0400

    -

--- server/app/controllers/trade.controller.js
+++ server/app/controllers/trade.controller.js
@@ -202,23 +202,25 @@ exports.pricecheck = async (req, res) => {
         console.log(error)
     }
 
-    res.send(
-        pcTrades
+    const filteredTrades = [];
+    for (const trade of pcTrades) {
+        const dataValues = trade.dataValues;
+        const query_pick = dataValues.draft_picks.find(
+            (pick) =>
+                pick.season === season &&
+                pick.round === round &&
+                (pick.order === order || !pick.order)
+        );
 
-            .map(trade => trade.dataValues)
-            .filter(trade => {
-                const query_pick = trade.draft_picks.find(
-                    pick => pick.season === season
-                        && pick.round === round
-                        && (pick.order === order || !pick.order)
-                )
-                return (
-                    Object.values(trade.adds).filter(x => x === trade.adds[req.body.player_id]).length === 1
-                    && !trade.draft_picks.find(pick => pick.new_user?.user_id === trade.adds[req.body.player_id])
-                ) || (
-                        trade.draft_picks.filter(pick => pick.new_user?.user_id === query_pick?.new_user?.user_id).length === 1
-                        && !Object.values(trade.adds).find(manager_user_id => manager_user_id === query_pick?.new_user?.user_id)
-                    )
-            })
-    )
+        if (
+            (Object.values(dataValues.adds).filter((x) => x === dataValues.adds[req.body.player_id]).length === 1 &&
+                !dataValues.draft_picks.find((pick) => pick.new_user?.user_id === dataValues.adds[req.body.player_id])) ||
+            (dataValues.draft_picks.filter((pick) => pick.new_user?.user_id === query_pick?.new_user?.user_id).length === 1 &&
+                !Object.values(dataValues.adds).find((manager_user_id) => manager_user_id === query_pick?.new_user?.user_id))
+        ) {
+            filteredTrades.push(dataValues);
+        }
+    }
+
+    res.send(filteredTrades);
 }
